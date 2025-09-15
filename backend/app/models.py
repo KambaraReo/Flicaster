369a818200eda6973ef178f6ae2c41c7833e4cb3
KambaraReo <r.kambara1997@gmail.com>
2025-09-16 00:34:11 +0900

    [add] モデルクラスの作成 #4

---
+++ backend/app/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, String, Float, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Airport(Base):
+  __tablename__ = "airports"
+
+  code = Column(String(10), primary_key=True)
+  name = Column(String(100), nullable=False)
+  region = Column(String(50), nullable=False)
+  scale = Column(String(50), nullable=False)
+  lat = Column(Float, nullable=False)
+  lon = Column(Float, nullable=False)
+
+class Flight(Base):
+  __tablename__ = "flights"
+
+  flight_no = Column(String(20), primary_key=True)
+  departure = Column(String(10), nullable=False)
+  arrival = Column(String(10), nullable=False)
+  airline_code = Column(String(10), nullable=False)
+  distance_km = Column(Float, nullable=False)
+  distance_cat = Column(String(20), nullable=False)
+  seat_capacity = Column(Integer, nullable=False)
